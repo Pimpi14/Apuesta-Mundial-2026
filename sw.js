dc17a44f6ac26e669f9116f2873034e0d86101bf
Pimpi14 <vivavision14@gmail.com>
2026-05-07 16:55:18 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'quiniela-v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(['/', '/index.html']);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
